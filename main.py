925050449b08453ee092e96a6fb6b534c3adf518
malbu <malbu@users.noreply.github.com>
2025-06-05 23:33:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class VideoStreamerApp:
 
     def _encode_and_send(self, frame):
         jpeg = encode_bgr_to_jpeg(frame, config.JPEG_QUALITY)
-        print('SEND → jpeg', len(jpeg), 'bytes')
+        print('SEND -> jpeg', len(jpeg), 'bytes')
         self.net.send_jpeg(jpeg)
 
 
@@ -38,7 +38,7 @@ class VideoStreamerApp:
         while self.running:
             data, ip = self.net.recv_datagram()
             if data:
-                print('RECV ←', len(data), 'bytes from', ip)
+                print('RECV <-', len(data), 'bytes from', ip)
                 self.proc.process_datagram(data, ip)
 
 
