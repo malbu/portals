0b7cf39a23b73b0823dcaf2d3b28e83136d91818
malbu <malbu@users.noreply.github.com>
2025-06-05 23:43:02 -0400

    added more debugging output

--- camera_manager.py
+++ camera_manager.py
@@ -21,12 +21,16 @@ class CameraManager:
         self.frame_time   = 1.0 / config.FPS_LIMIT if config.FPS_LIMIT else 0
         self.last_capture = 0.0
 
+        print('[INFO] capture thread running')
+
 
     def capture(self):
         now = time.time()
         if now - self.last_capture < self.frame_time:
             return None
         ret, frame = self.cap.read()
+        print('CAP ret=', ret, '; shape=',
+              None if frame is None else frame.shape)
         if not ret:
             return None
         self.last_capture = now
