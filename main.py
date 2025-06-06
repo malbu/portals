ede60336a21aea12aa83cfa85f6f08750c6327d2
malbu <malbu@users.noreply.github.com>
2025-06-05 23:50:40 -0400

    added try except to encode_bgr_to_jpeg on a hunch it was silently failing

--- main.py
+++ main.py
@@ -29,9 +29,13 @@ class VideoStreamerApp:
 
 
     def _encode_and_send(self, frame):
-        jpeg = encode_bgr_to_jpeg(frame, config.JPEG_QUALITY)
-        print('SEND -> jpeg', len(jpeg), 'bytes')
-        self.net.send_jpeg(jpeg)
+        print('ENC task start')
+        try:
+            jpeg = encode_bgr_to_jpeg(frame, config.JPEG_QUALITY)
+            print('SEND -> jpeg', len(jpeg), 'bytes')
+            self.net.send_jpeg(jpeg)
+        except Exception as e:
+            print('ENC task EXCEPTION', e)
 
 
     def _receiver_loop(self):
